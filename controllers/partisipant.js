4a5e6674956ffcc2e0b439a3c3c76b62ed8298f2
Firly Arie Azland <arieazland@gmail.com>
2021-06-11 14:09:37 +0700

    Update partisipant.js

--- controllers/partisipant.js
+++ controllers/partisipant.js
@@ -8,6 +8,17 @@ const MAIN_URL = require ("../urlconfig.js");
 exports.input = async (req, res, dataputs) => {
     try{
         const { selectpartisipant } = req.body;
+
+        if(selectpartisipant){
+
+        } else {
+          /** Field tidak boleh kosong */
+          req.session.sessionFlash = {
+            type: 'error',
+            message: 'Field tidak boleh kosong!'
+          }
+          res.redirect("/partisipant");
+        }
         
     } catch (err){
         console.log(err);
